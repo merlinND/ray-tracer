05d06cce1a04aa2ec4a925e1d6376d09050b2f03
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-05 11:29:06 +0200

    Notion of displayable pixel

--- src/Color.h
+++ src/Color.h
@@ -2,10 +2,17 @@
 #define COLOR_H
 
 #include <Eigen/Core>
+#include <time.h>
 
+/** RGB colors, each component ranging from 0 to 1 */
 typedef Eigen::Vector3f Color;
+/** RGB colors, each component ranging from 0 to 255 */
+typedef Eigen::Vector3i Pixel;
 
-Color clip(Color c, float min = 0, float max = 1) {
+/**
+ * Clip all components of the color to be within min and max
+ */
+inline Color clip(Color & c, float min = 0, float max = 1) {
   Color clipped = c;
   for(int i = 0; i < 3; ++i) {
     if(clipped[i] < min)
@@ -14,6 +21,27 @@ Color clip(Color c, float min = 0, float max = 1) {
       clipped[i] = max;
   }
   return clipped;
+}
+
+/**
+ * @return A randomly generated color.
+ * @TODO Use a better random generator
+ */
+inline Color getRandomColor() {
+  srand(clock());
+  return Color((float)rand(), (float)rand(), (float)rand());
+}
+
+/**
+ * Turn a color into a displayable pixel
+ */
+inline Pixel colorToPixel(Color & c) {
+  Color clipped = clip(c);
+  return Pixel(
+      (int)(clipped[0] * 255),
+      (int)(clipped[1] * 255),
+      (int)(clipped[2] * 255)
+    );
 }
 
 #endif
