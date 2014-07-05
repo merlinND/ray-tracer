86ab859e4fbbd139288544f1ba636edddb7a3dc7
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-05 18:54:44 +0200

    Appreciate perspective

--- src/scenes/TestScene.cpp
+++ src/scenes/TestScene.cpp
@@ -4,9 +4,9 @@
 #include "../geometry/Cube.h"
 
 TestScene::TestScene()
-  : Scene(Color(0, 1, 0), "Hello test scene") {
+  : Scene(Color(0.8, 0.93, 0.33), "Hello test scene") {
   addLightSource(new AmbientLight(Color(1, 0, 0)));
 
-  Point p(0, 0, 5);
+  Point p(0, 0, 1);
   addObject(new Cube(p, 0.5));
 }
