996d61f43493eb7d4b854fa828271a20f52653c2
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-05 15:41:55 +0200

    Pretty printing of rays

--- src/Ray.h
+++ src/Ray.h
@@ -1,6 +1,8 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include <iostream>
+
 #include "types.h"
 
 class Ray {
@@ -14,5 +16,15 @@ public:
 protected:
 
 };
+
+/**
+ * Pretty printing of rays
+ */
+inline std::ostream& operator<<(std::ostream& os, Ray const & r) {
+  os << "from (" << r.from[0] << "," << r.from[1] << "," << r.from[2] << ")";
+  os << " in direction ";
+  os << "(" << r.direction[0] << "," << r.direction[1] << "," << r.direction[2] << ")";
+  return os;
+}
 
 #endif
