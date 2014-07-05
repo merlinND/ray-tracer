c7b7e4c43d9b431cb905bb349ca72ea832590b9a
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-05 14:37:04 +0200

    Stubs for camera

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,15 +1,20 @@
 #include "Camera.h"
 
-/**
- * Default constructor
- */
-Camera::Camera() {
+float const Camera::DEFAULT_DISTANCE = 2;
+float const Camera::DEFAULT_FOV = 1.570796327;
+Point const Camera::DEFAULT_POSITION = Point();
+Point const Camera::DEFAULT_LOOK_AT = Point(0, 0, 1);
+
+Camera::Camera(Point const & p, Point const & l, float distance, float fieldOfView)
+  : position(p), lookAt(l), d(distance), fov(fieldOfView) {
+
+  computeViewMatrix();
+}
+
+Camera::~Camera() {
 
 }
 
-/**
- * Destructor
- */
-Camera::~Camera() {
-
+void Camera::computeViewMatrix() {
+  // TODO
 }
--- src/Camera.h
+++ src/Camera.h
@@ -1,14 +1,51 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <Eigen/Core>
+
+#include "types.h"
+
 class Camera
 {
 public:
-  Camera();
+  /** Physical position of the camera */
+  Point position;
+  /** Point at which the camera is looking at */
+  Point lookAt;
+  /** Distance from the camera to the screen (in meters) */
+  float d;
+  /** Field of view of the camera (in radians) */
+  float fov;
+
+  /**
+   * View matrix in homogeneous coordinates
+   * Allows to go from scene coordinates to camera coordinates
+   * by left matrix multiplication
+   * @TODO check that it's not the other way around...
+   */
+  Eigen::Matrix4f viewMatrix;
+
+  /** Default parameters */
+  static float const DEFAULT_DISTANCE;
+  static float const DEFAULT_FOV;
+  static Point const DEFAULT_POSITION;
+  static Point const DEFAULT_LOOK_AT;
+
+  /**
+   * @param p Position of the camera
+   * @param l Point at which the camera is looking at
+   */
+  Camera(Point const & p = DEFAULT_POSITION,
+         Point const & l = DEFAULT_LOOK_AT,
+         float distance = DEFAULT_DISTANCE,
+         float fieldOfView = DEFAULT_FOV);
   virtual ~Camera();
 
 protected:
-
+  /** Update `viewMatrix` based on
+   * position, lookAt, distance to screen and field of view
+   */
+  void computeViewMatrix();
 };
 
 #endif
