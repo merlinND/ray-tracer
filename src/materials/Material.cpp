8c1c50ee123d0ce3cfc9ce638e1c4e897010dd4d
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-07-20 17:51:41 +0200

    Material coefficients

--- src/materials/Material.cpp
+++ src/materials/Material.cpp
@@ -1,6 +1,15 @@
 #include "Material.h"
 
-Material::Material() {
+Material::Material(float dr, float sr, float se,
+                   float dt, float st,
+                   float ir, float it,
+                   float a,
+                   float n)
+  : diffuseReflection(dr), specularReflection(sr), specularExponent(se),
+    diffuseTransmission(dt), specularTransmission(st),
+    idealReflection(ir), idealTransmission(it),
+    ambientLight(a),
+    refractionIndex(n) {
 
 }
 
--- src/materials/Material.h
+++ src/materials/Material.h
@@ -8,11 +8,26 @@
  */
 class Material {
 public:
+  /**
+   * The following are coefficients defining the reaction of
+   * different kinds of materials (glass, mirror, plastic, ...)
+   * to light. Each coefficient is in [0, 1].
+   */
+  float diffuseReflection, specularReflection, specularExponent,
+        diffuseTransmission, specularTransmission,
+        idealReflection, idealTransmission,
+        ambientLight,
+        refractionIndex;
+
   /** Basic materials */
   static Material const PLASTIC;
 
   /** Constructor */
-  Material();
+  Material(float dr = 1, float sr = 0.5, float se = 5,
+           float dt = 0.5, float st = 0.5,
+           float ir = 0, float it = 0,
+           float a = 1,
+           float n = 1);
 
 protected:
 
