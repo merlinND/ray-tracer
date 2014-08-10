9f42ec51ec8c2bf9f55379e712c67fb49fb2447d
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-10 15:31:52 +0200

    Use Monte Carlo sampling in area lighting

--- src/lights/AreaLight.cpp
+++ src/lights/AreaLight.cpp
@@ -21,32 +21,33 @@ float AreaLight::getParticipation(Scene const & scene, Ray const & lightRay) {
 
   // We evaluate the surface visibility by making a number of tests
   // proportional to the area.
-  int n = ceil((this->side * this->side) * AreaLight::SAMPLING_DENSITY);
-
   // Tests are organized on a grid of side `sqrt(n)`,
   // so that the whole grid contains `n` test points
-  // TODO: Monte-Carlo sampling
-  int grid = sqrt(n);
+  int grid = sqrt(ceil((this->side * this->side) * AreaLight::SAMPLING_DENSITY));
+  int n = (grid * grid);
+
   // Here we assume that this light is parallel to (O, x, y)
   // TODO: cache this values to compute only once
   float halfSide = (this->side / 2.0f);
   float stride = (this->side / grid);
   Point corner = (this->position - Vec(halfSide, halfSide, 0));
-  Point target = corner;
+  Point target;
   Ray localRay(lightRay.from, target - lightRay.from);
 
   for(int i = 0; i < grid; ++i) {
     for(int j = 0; j < grid; ++j) {
+      // TODO: use a better random number generator
+      Vec offset = Vec((j + rand() / (float)RAND_MAX) * stride,
+                       (i + rand() / (float)RAND_MAX) * stride,
+                       0);
+      target = corner + offset;
+
       localRay.direction = (target - lightRay.from).normalized();
 
       if(!scene.isObstructed(localRay, target)) {
         visibleSamples++;
       }
-
-      target[0] += stride;
     }
-    target[0] -= side;
-    target[1] += stride;
   }
 
   float participation = (visibleSamples / (float)n);
