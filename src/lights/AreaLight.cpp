ba934c72200fe8967c07a6726c6c63180e015992
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-10 14:28:58 +0200

    Area lighting algorithm

--- src/lights/AreaLight.cpp
+++ src/lights/AreaLight.cpp
@@ -14,4 +14,41 @@ AreaLight::AreaLight(Point const & p, float s, float i, Color const & c)
 }
 
 float AreaLight::getParticipation(Scene const & scene, Ray const & lightRay) {
+  // This light covers a square surface.
+  // Our goal is to determine which portion of this surface
+  // is visible from `ray.from`.
+  int visibleSamples = 0;
+
+  // We evaluate the surface visibility by making a number of tests
+  // proportional to the area.
+  int n = ceil((this->side * this->side) * AreaLight::SAMPLING_DENSITY);
+
+  // Tests are organized on a grid of side `sqrt(n)`,
+  // so that the whole grid contains `n` test points
+  // TODO: Monte-Carlo sampling
+  int grid = sqrt(n);
+  // Here we assume that this light is parallel to (O, x, y)
+  // TODO: cache this values to compute only once
+  float halfSide = (this->side / 2.0f);
+  float stride = (this->side / grid);
+  Point corner = (this->position - Vec(halfSide, halfSide, 0));
+  Point target = corner;
+  Ray localRay(lightRay.from, target - lightRay.from);
+
+  for(int i = 0; i < grid; ++i) {
+    for(int j = 0; j < grid; ++j) {
+      localRay.direction = (target - lightRay.from).normalized();
+
+      if(!scene.isObstructed(localRay, target)) {
+        visibleSamples++;
+      }
+
+      target[0] += stride;
+    }
+    target[0] -= side;
+    target[1] += stride;
+  }
+
+  float participation = (visibleSamples / (float)n);
+  return participation;
 }
