1e356f3844d7a46938daa6b4ca6a8858983d928a
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-08-17 14:56:56 +0200

    Add specular transmission exponent property to materials

--- src/materials/Material.h
+++ src/materials/Material.h
@@ -14,7 +14,7 @@ public:
    * to light. Each coefficient is in [0, 1].
    */
   float diffuseReflection, specularReflection, specularExponent,
-        diffuseTransmission, specularTransmission,
+        diffuseTransmission, specularTransmission, specularTransmissionExponent,
         idealReflection, idealTransmission,
         ambientLight,
         refractionIndex;
@@ -28,8 +28,8 @@ public:
   static Material const MIRROR;
 
   /** Constructor */
-  Material(float dr = 1, float sr = 0.2, float se = 1,
-           float dt = 0.5, float st = 0.5,
+  Material(float dr = 1, float sr = 0.2, float se = 5,
+           float dt = 0.5, float st = 0.5, float ste = 15,
            float ir = 0, float it = 0,
            float a = 1,
            float n = 1);
