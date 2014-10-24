74f4cdc4ae5639ee0f91f7ea79632fff75a759d5
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-24 21:38:04 +0200

    Test with a procedurally generated texture

--- src/scenes/TexturesTestScene.cpp
+++ src/scenes/TexturesTestScene.cpp
@@ -2,16 +2,18 @@
 
 #include "../renderer/Color.h"
 #include "../lights/PunctualLight.h"
+#include "../lights/AreaLight.h"
 
 #include "../textures/GridTexture.h"
 #include "../textures/ImageTexture.h"
 
 #include "../geometry/Plane.h"
 #include "../geometry/Cube.h"
+#include "../geometry/Parallelepipoid.h"
 #include "../geometry/Sphere.h"
 
 TexturesTestScene::TexturesTestScene()
-  : Scene(getRandomColor(), "Texture test scene") {
+  : Scene(Colors::WHITE, "Texture test scene") {
 
   // Lighting
   PunctualLight * light = new PunctualLight(Point(0, 0, 4),
@@ -26,23 +28,29 @@ TexturesTestScene::TexturesTestScene()
   // Walls
   p << 0, 0, -3;
   o = new Plane(p, Vec(0, 0, 1));
-  o->setTexture(new GridTexture());
+  // o->setTexture(new GridTexture());
   addObject(o);
 
-  p << 0, -2, 0;
+  p << 0, 0, 0;
   o = new Sphere(p, 1);
-  o->setTexture(new ImageTexture("./res/textures/garden.jpg"));
+  o->setTexture(new ImageTexture("./res/textures/orange-sand.jpg"));
   o->rotate(- PI / 2.f, 0.1f, 0);
   addObject(o);
 
-  p << -2, 0, 0;
-  o = new Sphere(p, 1);
-  o->setTexture(new ImageTexture("./res/textures/rock.jpg"));
-  addObject(o);
+  // p << -2.5, 0, 0;
+  // o = new Parallelepipoid(p, 0.1, 5, 5);
+  // o->setMaterial(Material::MIRROR);
+  // o->rotate(0, -0.4, 0);
+  // addObject(o);
 
-  p << 0.1, 0.1, 0;
-  o = new Cube(p, 1.2);
-  o->setTexture(new ImageTexture("./res/textures/rubiks-cube.jpg"));
-  addObject(o);
+  // p << -2, 0, 0;
+  // o = new Sphere(p, 1);
+  // o->setTexture(new ImageTexture("./res/textures/rock.jpg"));
+  // addObject(o);
+
+  // p << 0.1, 0.1, 0;
+  // o = new Cube(p, 1.2);
+  // o->setTexture(new ImageTexture("./res/textures/rubiks-cube.jpg"));
+  // addObject(o);
 
 }
